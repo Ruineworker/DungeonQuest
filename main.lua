2ce874088e58b6748565e6b014b2df9e327b7671
Ruineworker <83076802+Ruineworker@users.noreply.github.com>
2025-08-26 19:51:55 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -109,7 +109,7 @@ function Functions:Teleport(Cframe)
     Character.HumanoidRootPart.Anchored = false
     repeat task.wait()
         if Character:FindFirstChild("HumanoidRootPart") and bodyPosition ~= nil and bodyGyro ~= nil then
-            Character:PivotTo(CFrame.new(Cframe.p + Vector3.new(0, Settings.AutoFarm.Distance * 2, 0))* CFrame.Angles(math.rad(90), 0, 0))
+            Character:PivotTo(CFrame.new(Cframe.p + Vector3.new(0, Settings.AutoFarm.Distance * 2, 0))* CFrame.Angles(math.rad(270), 0, 0))
             bodyPosition.Position = Cframe.Position + Vector3.new(0, Settings.AutoFarm.Distance * 2, 0)
             bodyGyro.CFrame = CFrame.new(Character:GetPivot().p, Cframe.Position) * CFrame.Angles(math.rad(90), 0, 0)
         end
